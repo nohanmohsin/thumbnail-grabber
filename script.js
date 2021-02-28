cce2c80d59ee277ac7ba0596f85c513963ac2bc7
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2021-02-28 17:16:26 +0600

    another job done

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ btn.addEventListener('click', () => {
     if(input.length > 42 && input.includes('https://www.youtube.com/watch?v=')){
         const index = input.indexOf('=');
         const videoId = input.slice(index+1, index+12);
+
         url=`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
         console.log(url);
         const image = document.createElement('img');
         image.src = url;
-        document.getElementById('input').style.display= 'none';
-        
-        const ytApiKey = 'bonk';
+        image.className= 'searched-image';
+        const ytApiKey = 'if you trynna see if I put this here then bonk...if not still bonk';
         fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoId + "&key=" + ytApiKey)
         .then(res => res.json())
         .then(data => {
@@ -20,12 +20,16 @@ btn.addEventListener('click', () => {
             const nameOfVid = document.getElementById("title");
             nameOfVid.innerText= title;
             if(title.length > 0){
+                document.getElementById('input').style.display= 'none';
                 document.getElementById('img').appendChild(image);
             }
             
         });
-        console.log(title);
         
+    }
+
+    else{
+        alert("it aint it bub...do it right");
     }
 
 })
