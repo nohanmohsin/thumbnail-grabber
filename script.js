f98c8dd9e80490ec0c81de40c241cfe7c5eb30b0
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2021-03-01 22:04:02 +0600

    yesnt

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ btn.addEventListener('click', () => {
         const image = document.createElement('img');
         image.src = url;
         image.className= 'searched-image';
-        const ytApiKey = process.env.API_KEY;//bonk
+        const ytApiKey = env.API_KEY;//bonk
         fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoId + "&key=" + ytApiKey)
         .then(res => res.json())
         .then(data => {
